1ebf16fa651d1b03168f3217877efa293ea72235
Elias Martenson <lokedhs@gmail.com>
2014-09-11 22:55:14 +0800

    Added locks.lisp

---
+++ src/locks.lisp
@@ -0,0 +1,27 @@
+(in-package :containers)
+
+(defclass lockable-instance ()
+  ((lock          :type t
+                  :initform (bordeaux-threads:make-lock)
+                  :reader lockable-instance/lock)
+   (cond-variable :type t
+                  :initform (bordeaux-threads:make-condition-variable)
+                  :reader lockable-instance/cond-variable))
+  (:documentation "Sequence that supports blocking on empty queues"))
+
+(defmethod initialize-instance :after ((obj lockable-instance) &key lockable-instance-name)
+  (setf (slot-value obj 'lock) (bordeaux-threads:make-lock lockable-instance-name))
+  (setf (slot-value obj 'cond-variable) (bordeaux-threads:make-condition-variable :name lockable-instance-name)))
+
+(defmacro with-locked-instance (obj &body body)
+  `(bordeaux-threads:with-recursive-lock-held ((lockable-instance/lock ,obj))
+     ,@body))
+
+(defmacro with-locked-instance-maybe (obj &body body)
+  (let ((obj-sym (gensym "OBJ-"))
+        (body-sym (gensym "BODY-")))
+    `(let ((,obj-sym ,obj))
+       (flet ((,body-sym () ,@body))
+         (if (typep ,obj-sym 'lockable-instance)
+             (with-locked-instance ,obj-sym (,body-sym))
+             (,body-sym))))))
